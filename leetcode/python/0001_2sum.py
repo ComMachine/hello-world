569561bb42de549f58faba90f466c1cb3d10b503
ComMachine <wuning@rocketmail.com>
2016-08-14 14:03:49 -0400

    Update 0001_2sum.py

--- leetcode/python/0001_2sum.py
+++ leetcode/python/0001_2sum.py
@@ -9,10 +9,14 @@ class Solution(object):
         l = []
         
         for i in xrange(0, len(nums)):
-            for j in xrange(0, len(nums)):
-                if (i==j):
-                    continue
+            for j in xrange (i+1, len(nums)):
+            # no need to handle the special case that i==j, can start from i+1
+            #for j in xrange(0, len(nums)):
+                #if (i==j):
+                #    continue
                 if (nums[i]+nums[j] == target):
-                    l.append(i)
-                    l.append(j)
-                    return l
+                    #l.append(i)
+                    #l.append(j)
+                    #return l
+                    # this can be one line
+                    return (i, j)
